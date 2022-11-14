0dc3e66acfcbd68097511c7f91d1eae7d906c98f
qqKostya <leap74rus@gmail.com>
2022-11-14 16:21:30 +0300

    add: pagination and lodash

---
+++ src/app/components/users.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Pagination from "./pagination";
+import User from "./user";
+const Users = ({ users, ...rest }) => {
+  const count = users.length;
+  const pageSize = 4;
+  const [currentPage, setCurrentPage] = useState(1);
+  const handlePageChange = (pageIndex) => {
+    console.log("page", pageIndex);
+    setCurrentPage(pageIndex);
+  };
+  return (
+    <>
+      {count > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Провфессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th scope="col">Избранное</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <User key={user._id} {...rest} {...user} />
+            ))}
+          </tbody>
+        </table>
+      )}
+      <Pagination
+        itemsCount={count}
+        pageSize={pageSize}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </>
+  );
+};
+
+export default Users;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Users from './components/users';
+import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./app/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Users />
-  </React.StrictMode>
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
